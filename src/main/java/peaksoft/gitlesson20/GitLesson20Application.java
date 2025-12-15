6683131e67d50dadd6179368f8ea410763dfcb3c
as-aijamal <a.asangazieva@gmail.com>
2025-12-15 12:16:38 +0600

    Initial commit

---
+++ src/main/java/peaksoft/gitlesson20/GitLesson20Application.java
@@ -0,0 +1,39 @@
+package peaksoft.gitlesson20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLesson20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitLesson20Application.class, args);
+        System.out.println("Hello Java-20");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/peaksoft/gitlesson20/GitLesson20ApplicationTests.java
@@ -0,0 +1,13 @@
+package peaksoft.gitlesson20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLesson20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
